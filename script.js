ee4d099926378d3987126516841d7da4a6dfe893
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-18 14:40:49 +0600

    added new element

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let h1 = document.querySelector('h1');
 
 document.querySelector('#text').addEventListener('input', function () {
-   h1.textContent = this.valie;
+   h1.textContent = this.value;
+});
+
+
+document.querySelector('#color').addEventListener('input', function() {
+   h1.style.color = this.value
+});
+document.querySelector('#').addEventListener('input', function() {
+
+});
+document.querySelector('#font-family').addEventListener('input', function () {
+   h1.style.fontFamily = 
 });
