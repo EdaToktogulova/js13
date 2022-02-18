b9611283222805c1406d3b00b76c0cc8d7e5cfea
Eda Toktogulova <edatoktogulova2003@gmail.com>
2022-02-18 14:51:16 +0600

    new element

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ document.querySelector('#text').addEventListener('input', function () {
    h1.textContent = this.value;
 });
 
-
 document.querySelector('#color').addEventListener('input', function() {
    h1.style.color = this.value
 });
 document.querySelector('#').addEventListener('input', function() {
-   
+   h1.style.backgroundColor = this.value;
 });
+
 document.querySelector('#font-family').addEventListener('input', function () {
-   h1.style.fontFamily = ("verdana")
+   h1.style.fontFamily = this.value;
+});
+
+document.querySelector('#').addEventListener('input', function () {
+   h1.style.fontSize = this.value;
 });
